daf0a299d685b104a9951352061c956e0f208530
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2023-07-23 14:10:56 +0200

    Sync with polygon

--- ioi2011-elephants/src/validate.cpp
+++ ioi2011-elephants/src/validate.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
-int main(){
-    registerValidation();
-    int n = inf.readInt(1,150000);
+int MAXN[] = {150'000, 2, 100, 50'000, 70'000, 150'000};
+int MAXM[] = {150'000, 100, 100, 50'000, 70'000, 150'000};
+
+int main(int argc, char **argv){
+    registerValidation(argc, argv);
+    int group = atoi(validator.group().c_str());
+    int n = inf.readInt(1, MAXN[group]);
     inf.readSpace();
     inf.readInt(1,1000000000);
     inf.readSpace();
-    int m = inf.readInt(1,150000);
+    int m = inf.readInt(1, MAXM[group]);
     inf.readEoln();
 
     int last = 0;
